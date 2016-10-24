81077250b9276377b66db3de15c1f56ded238bd7
Eric Lee <cireL92@gmail.com>
2016-10-24 13:40:44 -0700

    Update the gameover to use space.
    Stops the player from accidentally skipping into a new game.

--- click_the_circle/gameOver.js
+++ click_the_circle/gameOver.js
@@ -7,15 +7,22 @@ var gameOverState = {
   create: function(){
     this.game.stage.backgroundColor = '#71c5cf';
 
-    this.gameTitle = this.game.add.text(20,275, "Click anywhere to try again!",
+    // this.gameTitle = this.game.add.text(20,275, "Click anywhere to try again!",
+    //   {font: "30px Arial", fill: "#ffffff"});
+
+    this.gameTitle = this.game.add.text(35,275, "Press Space to try again!",
       {font: "30px Arial", fill: "#ffffff"});
+
+    // Listen on the space input to start the game.
+    var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+    spaceKey.onDown.add(this.startGame, this);
 
   },
 
   update: function(){
-    if (this.game.input.activePointer.isDown){
-      this.startGame();
-    }
+    // if (this.game.input.activePointer.isDown){
+    //   this.startGame();
+    // }
   },
 
   startGame: function(){
